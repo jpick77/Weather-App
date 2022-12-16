b77fa14039b03854596cda4bcf445da7734344bc
Jacob Pickron <jpickron30@gmail.com>
2022-12-16 13:18:36 -0700

    got the icons to show on the page

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+// console.log(currentDay);
+
+// let day = currentDay.getDate();
+// let month = currentDay.getMonth();
+// let year = currentDay.getFullYear();
+
+// let currentDate = `${month}-${day}-${year}`;
+// console.log(currentDate);
+
+
 let submitButton = document.getElementById("submit");
 
 function getWeather(query) {
@@ -18,18 +29,46 @@ function displayCurrentWeather(weather) {
     city.textContent = weather.name;
     console.log(city);
 
+    // let dateEl = document.getElementById("currentDay");
+    // dateEl.textContent = weather.dt;
+    
+    // console.log(dateEl);
+
+    const unixTimestamp = weather.dt;
+
+    const milliseconds = unixTimestamp * 1000;
+    
+    const dateObject = new Date(milliseconds);
+    
+    const humanDateFormat = dateObject.toLocaleString(); //2019-12-9 10:30:15
+    console.log(humanDateFormat);
+    dateObject.toLocaleString("en-US", {weekday: "long"}); // Monday
+    dateObject.toLocaleString("en-US", {month: "long"}); // December
+    dateObject.toLocaleString("en-US", {day: "numeric"}); // 9
+    dateObject.toLocaleString("en-US", {year: "numeric"}); // 2019
+    // dateObject.toLocaleString("en-US", {hour: "numeric"}); // 10 AM
+    // dateObject.toLocaleString("en-US", {minute: "numeric"}); // 30
+    // dateObject.toLocaleString("en-US", {second: "numeric"}); // 15
+    // dateObject.toLocaleString("en-US", {timeZoneName: "short"}); // 12/9/2019, 10:30:15 AM CST
+
     let temp = document.getElementById("currentTemp");
-    temp.textContent = "Temp: " + weather.main.temp + " F";
+    temp.textContent = "Temp: " + weather.main.temp + " °F";
     console.log(temp);
 
     let wind = document.getElementById("currentWind");
-    wind.textContent = "Wind: " + weather.wind.speed + " mph, " + weather.wind.deg + " ° ";
+    wind.textContent = "Wind: " + weather.wind.speed + " MPH";
     console.log(wind);
 
     let humidity = document.getElementById("currentHumidity");
     humidity.textContent = "Humidity: " + weather.main.humidity + " %";
     console.log(humidity);
 
+    
+    iconId = weather.weather[0].icon;
+    let iconElement = document.getElementById("iconOne");
+    iconElement.innerHTML = `<img src="http://openweathermap.org/img/w/${iconId}.png"/>`;
+
+    console.log(iconElement);
 };
 
 function getFiveForcast() {
@@ -44,3 +83,5 @@ function getFiveForcast() {
 };
 
 submitButton.addEventListener("click", getFiveForcast);
+
+    
